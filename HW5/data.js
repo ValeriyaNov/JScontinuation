52d9ea1554b161e33a9677394e825e1c6bfd232b
ValeriyaNow <wal.now@yandex.ru>
2023-11-24 22:30:57 +0300

    hw5

---
+++ HW5/data.js
@@ -0,0 +1,39 @@
+const dataInfo = `
+[
+    {
+    "id": 1,
+    "title": "Pancakes",
+    "img": "https://images.unsplash.com/photo-1612182062633-9ff3b3598e96?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGFuY2FrZXN8ZW58MHx8MHx8fDA%3D",
+    "price": "78"
+  },
+  {
+    "id": 2,
+    "title": "Cake",
+    "img": "https://images.unsplash.com/photo-1562440499-64c9a111f713?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y2FrZXxlbnwwfHwwfHx8MA%3D%3D",
+    "price": "78"
+  },
+  {
+    "id": 3,
+    "title": "Croissant",
+    "img": "https://images.unsplash.com/photo-1600521853186-93b88b3a07b0?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNyb2lzc2FudHxlbnwwfHwwfHx8MA%3D%3D",
+    "price": "89"
+  },
+  {
+    "id": 4,
+    "title": "Ice cream",
+    "img": "https://images.unsplash.com/photo-1563805042-7684c019e1cb?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGljZSUyMGNyZWFtfGVufDB8fDB8fHww",
+    "price": "78"
+  },
+  {
+    "id": 5,
+    "title": "Waffles",
+    "img": "https://images.unsplash.com/photo-1542692422-5cb6fc2cf737?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHdhZmZsZXN8ZW58MHx8MHx8fDA%3D",
+    "price": "178"
+  },
+  {
+    "id": 6,
+    "title": "Donut",
+    "img": "https://images.unsplash.com/photo-1551024601-bec78aea704b?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZG9udXR8ZW58MHx8MHx8fDA%3D",
+    "price": "8"
+  }
+]`
